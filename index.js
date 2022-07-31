136a53a38bf77fa2c1bc92988b3aa8279544eeed
bd-dev-ismail <ismailratul7@gmail.com>
2022-07-31 22:20:24 +0600

    More index

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
-var numbers = [23, 43, 54, 64, 66];
-//get element by index
+// var numbers = [23, 43, 54, 64, 66];
+// //get element by index
+// var element = numbers[1];
+// console.log(element);
+// //set elemtn values by index
+// numbers[1] = 34;
+// numbers[3] = 59;
+// console.log(numbers);
+// //find index of element{Give me the index of 54 & 66 froms array}
+// var positionIndex = numbers.indexOf(54);
+// var position = numbers.indexOf(66);
+// console.log(positionIndex);
+// console.log(position);
+var numbers = [22, 33, 44, 55, 66, 77, 88, 99];
+var element = numbers[4];
+// console.log(element);
+numbers[4] = 110;
+// console.log(numbers);
+var positionIndex = numbers.indexOf(99);
+console.log(positionIndex);
