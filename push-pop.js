25704914a0b8aeb8f926156a67777afade58749b
bd-dev-ismail <ismailratul7@gmail.com>
2022-07-31 22:48:39 +0600

    Teaching push & pop

---
+++ push-pop.js
@@ -0,0 +1,15 @@
+var numbers = [22, 15, 35, 56, 52];
+//Use push() to add element to an array  at the last element 
+// numbers.push(99);
+// console.log(numbers);
+var friendsName = ['Sabbir', 'Tushar', 'Antor', 'Nafisa'];
+
+friendsName.push('Ismail');
+friendsName.push('Ratul');
+friendsName.push('Nowrin');
+// console.log(friendsName);
+//Using pop to get the last elemetns 
+// numbers.pop();
+var element = numbers.pop();
+console.log(numbers);
+console.log(element);
